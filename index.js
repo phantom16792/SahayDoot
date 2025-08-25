8e542ea5c0d60d3f798be789b7b6a3cecd1ea5dc
phantom16792 <aryanmhaiskar51@gmail.com>
2025-08-26 01:07:05 +0530

    added live location feature in js and logged that data in console

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 let emergency_btn = document.getElementById("emer_btn");
 
-function gotlocation (location){
-    
-    console.log(location.coords);
-
+function gotlocation(location) {
+  console.log(
+    `latitude: ${location.coords.latitude}\nlongitude: ${location.coords.longitude}`
+  );
+  // Usage
+  getLocationFromCoords(
+    location.coords.latitude,
+    location.coords.longitude
+  ).then((location) => console.log(location));
 }
 
-function nolocation (){
-    console.log("error");
+function nolocation() {
+  console.log("error");
 }
 
-emergency_btn.addEventListener("click", () =>{
-
-    navigator.geolocation.getCurrentPosition(gotlocation, nolocation);
+emergency_btn.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(gotlocation, nolocation);
 });
 
+async function getLocationFromCoords(lat, lon) {
+  try {
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
